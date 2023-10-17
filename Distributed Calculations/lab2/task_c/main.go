6230237a72e21761058e5eba2ad34802598342d6
dmytromk <dmytromk@gmail.com>
2023-10-17 22:12:26 +0300

    fix task_c in lab2 DC

--- Distributed Calculations/lab2/task_c/main.go
+++ Distributed Calculations/lab2/task_c/main.go
@@ -3,73 +3,52 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sync"
 	"time"
 )
-
-func main() {
-	rand.Seed(time.Now().UnixNano())
-
-	strengthRanks := []int{10, 8, 6, 12, 14, 16, 20, 18}
-
-	var wg sync.WaitGroup
-
-	finalWinner := playTournament(strengthRanks, &wg)
-
-	wg.Wait()
-
-	fmt.Printf("The final winner is Player %d with a strength rank of %d\n", finalWinner.index, finalWinner.strengthRank)
-}
 
 type Player struct {
 	index        int
 	strengthRank int
 }
 
-func playGame(player1, player2 Player, resultCh chan<- Player, wg *sync.WaitGroup) {
-	defer wg.Done()
+func main() {
+	rand.Seed(time.Now().UnixNano())
 
-	winner := player1
-	if player2.strengthRank > player1.strengthRank {
-		winner = player2
-	}
+	strengthRanks := []int{10, 8, 6, 12, 14, 16, 20, 18, 5} // Example with 9 players
 
-	resultCh <- winner
+	finalWinner := playTournament(strengthRanks)
+
+	fmt.Printf("The final winner is Player %d with a strength rank of %d\n", finalWinner.index, finalWinner.strengthRank)
 }
 
-func playTournament(players []int, wg *sync.WaitGroup) Player {
-	resultCh := make(chan Player, len(players)/2)
+func playGame(player1, player2 Player) Player {
+	if player1.strengthRank > player2.strengthRank {
+		return player1
+	}
+	return player2
+}
 
-	for i := 0; i < len(players)/2; i++ {
-		player1 := Player{index: i * 2, strengthRank: players[i*2]}
-		player2 := Player{index: i*2 + 1, strengthRank: players[i*2+1]}
-
-		wg.Add(1)
-
-		go playGame(player1, player2, resultCh, wg)
+func playTournament(players []int) Player {
+	var playerList []Player
+	for i, strength := range players {
+		playerList = append(playerList, Player{index: i, strengthRank: strength})
 	}
 
-	var winners []Player
-
-	for winner := range resultCh {
-		winners = append(winners, winner)
-		fmt.Printf("Game: Player %d vs. Player %d -> Winner: Player %d\n", winner.index-1, winner.index, winner.index)
-
-		if len(winners) == 2 {
-			wg.Add(1)
-
-			go func() {
-				defer wg.Done()
-				playGame(winners[0], winners[1], resultCh, wg)
-			}()
-
-			winners = nil
+	for len(playerList) > 1 {
+		var nextRound []Player
+		for i := 0; i < len(playerList); i += 2 {
+			if i+1 < len(playerList) {
+				winner := playGame(playerList[i], playerList[i+1])
+				nextRound = append(nextRound, winner)
+				fmt.Printf("Game: Player %d vs. Player %d -> Winner: Player %d\n", playerList[i].index, playerList[i+1].index, winner.index)
+			} else {
+				// Handle bye for odd player count
+				nextRound = append(nextRound, playerList[i])
+				fmt.Printf("Game: Player %d has a bye in this round.\n", playerList[i].index)
+			}
 		}
-
-		if len(winners) == 1 {
-			return winners[0]
-		}
+		playerList = nextRound
 	}
 
-	return playTournament(players, wg)
+	return playerList[0]
 }
