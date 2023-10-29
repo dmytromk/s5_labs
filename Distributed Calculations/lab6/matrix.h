71adfe291882b83f2aa3b858888ca1e9728856b3
dmytromk <dmytromk@gmail.com>
2023-10-29 18:06:33 +0200

    divided matrix into header and source lab6 DC

--- Distributed Calculations/lab6/matrix.h
+++ Distributed Calculations/lab6/matrix.h
@@ -25,68 +25,24 @@ namespace SquareMatrix {
     // Is stored as a 1D array with 9 elements:
     // [ 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 ]
 
-    double* create(unsigned int size) {
-        double* matrix = new double[size * size];
-        return matrix;
-    }
+    // Function to create a square matrix
+    double* create(unsigned int size);
 
     // Function to delete a square matrix
-    void destroy(double* matrix) {
-        delete[] matrix;
-    }
-
-    // Function to initialize a square matrix with zeros
-    void initialize(double* matrix, unsigned int size) {
-        for (unsigned int i = 0; i < size * size; i++) {
-            matrix[i] = 0.0;
-        }
-    }
+    void destroy(double* matrix);
 
     // Function to set a specific element in the matrix
-    void set(double* matrix, unsigned int size, unsigned int row, unsigned int col, double value) {
-        if (row < size && col < size) {
-            matrix[row * size + col] = value;
-        } else {
-            std::cerr << "Invalid row or column index." << std::endl;
-        }
-    }
+    void set(double* matrix, unsigned int size, unsigned int row, unsigned int col, double value);
 
     // Function to get a specific element from the matrix
-    double get(const double* matrix, unsigned int size, unsigned int row, unsigned int col) {
-        if (row < size && col < size) {
-            return matrix[row * size + col];
-        } else {
-            std::cerr << "Invalid row or column index." << std::endl;
-            return 0.0;
-        }
-    }
+    double get(const double* matrix, unsigned int size, unsigned int row, unsigned int col);
 
     // Function to fill the matrix with elements
-    void fill(double* matrix, unsigned int size, double element) {
-        unsigned int square = size * size;
-        for (unsigned int i = 0; i < square; i++) {
-            matrix[i] = element;
-        }
-    }
+    void fill(double* matrix, unsigned int size, double element);
 
     // Function to transpose the square matrix in-place
-    void transpose(double* matrix, unsigned int size) {
-        for (unsigned int i = 0; i < size; i++) {
-            for (unsigned int j = i + 1; j < size; j++) {
-                double temp = matrix[i * size + j];
-                matrix[i * size + j] = matrix[j * size + i];
-                matrix[j * size + i] = temp;
-            }
-        }
-    }
+    void transpose(double* matrix, unsigned int size);
 
     // Function to print the matrix
-    void print(const double* matrix, unsigned int size) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                std::cout << get(matrix, size, i, j) << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
+    void print(const double* matrix, unsigned int size);
 }
