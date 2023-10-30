5878b995ab6afc7de46f3055d24b88fdf18cbf9e
dmytromk <dmytromk@gmail.com>
2023-10-30 02:25:06 +0200

    started Cannon Multiplication

--- Distributed Calculations/lab6/src/CannonMultiplication.h
+++ Distributed Calculations/lab6/src/CannonMultiplication.h
@@ -4,20 +4,39 @@
 
 #pragma once
 
-#include <iostream>
 #include <time.h>
 #include <mpi.h>
+#include <string.h>
 
 #include "matrix.h"
 
-namespace CannonMultiplication {
-    MPI::Comm
-    MPI_Comm ColComm;
+int gridCoords[2];
+MPI_Comm col_comm;
+MPI_Comm col_row;
 
-    // Комунікатор для ствпчиків потоків
-    private static Cartcomm RowComm;
+// Розподілення блоків між потоками
+void matrixScatter(double *matrix, double *matrixBlock, unsigned int matrix_size, int block_size) {
+    int *matrix_row = (double *)malloc(sizeof(double) * block_size * matrix_size);
 
-    void calculate(unsigned int matrix_size) {
+    if (gridCoords[1] == 0)
+        MPI_Scatter(matrix, block_size * matrix_size, MPI_DOUBLE, matrix_row,
+                    block_size * matrix_size, MPI_DOUBLE, 0, col_comm);
 
+    for (int i = 0; i < block_size; i++) {
+        double* sub_row = (double*)malloc(sizeof(double) * block_size);
+        double *sub_row_result = (double *)malloc(sizeof(double) * block_size);
+
+        memcpy(sub_row, matrix_row + i * matrix_size, block_size * sizeof(int));
+
+        MPI_Scatter(sub_row, block_size, MPI_DOUBLE, sub_row_result, block_size, MPI_DOUBLE, 0, col_row);
+        memcpy(matrixBlock + i * block_size, sub_row_result, sizeof(double) * block_size);
+
+        free(sub_row_result);
     }
+
+    free(matrix_row);
+}
+
+void calculate(unsigned int matrix_size) {
+
 }
