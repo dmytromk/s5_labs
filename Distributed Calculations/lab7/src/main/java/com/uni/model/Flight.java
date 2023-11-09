43c407b6a0cc5c2d1651c88df4de674aa5eace4c
dmytromk <dmytromk@gmail.com>
2023-11-09 21:49:04 +0200

    add airline id for flight lab7 DC

--- Distributed Calculations/lab7/src/main/java/com/uni/model/Flight.java
+++ Distributed Calculations/lab7/src/main/java/com/uni/model/Flight.java
@@ -7,16 +7,18 @@ import lombok.Setter;
 @Getter
 public class Flight {
     private final String id;
+    private String airlineId;
     private String name;
     private String origin;
     private String destination;
     private Double price;
 
-    public Flight(String id, String name, String origin, String destination, Double price) {
+    public Flight(String id, String name, String origin, String destination, Double price, String airlineId) {
         this.id = id;
         this.name = name;
         this.origin = origin;
         this.destination = destination;
         this.price = price;
+        this.airlineId = airlineId;
     }
 }
