722c4193d1addf28cd1098d16c4418abca811ce6
dmytromk <dmytromk@gmail.com>
2023-11-23 00:08:52 +0200

    add usp and fps calculation

--- OOP/lab3/app/src/main/java/com/dmytromk/asteroids/GameLoop.java
+++ OOP/lab3/app/src/main/java/com/dmytromk/asteroids/GameLoop.java
@@ -4,6 +4,13 @@ import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 
 public class GameLoop extends Thread {
+    private final static double MAX_UPS = 120.0;
+    // times for one update in milliseconds
+    private final static double UPS_PERIOD = 1000 / MAX_UPS;
+
+    private double averageUPS;
+    private double averageFPS;
+
     private GameView gameView;
     private SurfaceHolder surfaceHolder;
     private boolean isRunning = false;
@@ -15,11 +22,11 @@ public class GameLoop extends Thread {
     }
 
     public double getAverageUPS() {
-        return 0;
+        return averageUPS;
     }
 
     public double getAverageFPS() {
-        return 0;
+        return averageFPS;
     }
 
     public void startLoop() {
@@ -31,8 +38,16 @@ public class GameLoop extends Thread {
     public void run() {
         super.run();
 
+        int updateCount = 0;
+        int frameCount = 0;
+
+        long startTime;
+        long elapsedTime;
+        long sleepTime;
+
         // Actual loop
         Canvas canvas;
+        startTime = System.currentTimeMillis();
         while (isRunning) {
             try {
                 canvas = surfaceHolder.lockCanvas();
@@ -41,6 +56,36 @@ public class GameLoop extends Thread {
                 surfaceHolder.unlockCanvasAndPost(canvas);
             } catch (IllegalArgumentException e) {
                 e.printStackTrace();
+            }
+
+            updateCount++;
+            frameCount++;
+
+            elapsedTime = System.currentTimeMillis() - startTime;
+            sleepTime = (long) (updateCount * UPS_PERIOD - elapsedTime);
+            if (sleepTime > 0) {
+                try {
+                    sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            while(sleepTime < 0 && updateCount < MAX_UPS - 1) {
+                this.gameView.update();
+                updateCount++;
+                elapsedTime = System.currentTimeMillis() - startTime;
+                sleepTime = (long) (updateCount*UPS_PERIOD - elapsedTime);
+            }
+
+            elapsedTime = System.currentTimeMillis() - startTime;
+            if (elapsedTime >= 1000) {
+                this.averageUPS = updateCount / (elapsedTime / 1000.0);
+                this.averageFPS = frameCount / (elapsedTime / 1000.0);
+
+                updateCount = 0;
+                frameCount = 0;
+                startTime = System.currentTimeMillis();
             }
         }
     }
